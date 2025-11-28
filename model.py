be9c89f659237531321d02a68efa770470c3f00d
LandonPyko <pykomar2@gmail.com>
2025-11-28 14:01:05 -0600

    Created sequences for the model to view

--- model.py
+++ model.py
@@ -2,44 +2,66 @@ from keras.layers import Dense
 import pandas as pd
 from tcn import TCN
 import os
+import numpy as np
 
 # Temporal Convolutional Network
 # Built for time series
 
 
 def process_data():
-    raw_data = []  # Holds list of pandas dataframes for each raw 
+    # just processing one file for now
 
-    fast = os.listdir("data_cleaning/data/fast")
-    for file in fast:
-        print(file)
-        fullPath = os.path.join("data_cleaning/data/fast",file)
-        df = pd.read_csv(fullPath)
-        raw_data.append(df)
+    mean = np.loadtxt("data_cleaning/time_series_data_64/Indesit/2_1647850500_1647854400_fast_means.txt")
+    median = np.loadtxt("data_cleaning/time_series_data_64/Indesit/2_1647850500_1647854400_fast_medians.txt")
 
-    stream_data = pd.read_csv("data_cleaning/data/stream_labels.csv")
-    print(stream_data)
+    data = np.column_stack((mean,median))  # Array containing mean and median for each window
+    windows = create_windows(data)
+    print(windows.shape)
 
     
+def create_windows(data):
+
+    windows = []
+
+    num_points = data.shape[0]
+    sequence = 64
+    stride = 16   # Allows for overlap to view patterns
+    for start in range(0, num_points - sequence + 1, stride):
+        seq = data[start:start+sequence]
+        windows.append(seq)
+    
+    return np.array(windows)
 
 
 def main():
     time = 0   # 
 
     process_data()
+    '''
+    window_size = 64
+    num_features = 2
+    model = Sequential([
+        TCN(
+            nb_filters = 64,
+            kernel_size = 3,
+            dilations = [1,2,4,8,16,32,64],
+            dropout_rate = .05,
+            return_sequences = False,
+            input_shape=(window_size,num_features)
+            ),
 
-    #model = Sequential([
-     #   TCN(input_shape=(time,8)),  # Length and dimension of input
-      #  Dense(1)  # Dense layer with one value as output (?)
-    #])
+        Dense(1,activation=None)  # Dense layer with one value as output (?)
+    ])
+    
 
     #model.compile(optimizer='adam',loss='mse')
 
-    # model.fit()
+    #model.fit()
 
-    # model.evaluate()
+    #model.evaluate()
 
-    # model.predict()
+    #model.predict()
+    '''
 
 if __name__ == "__main__":
     main()
